1324c20561c096cbf14eb7be3c76cceb046682e4
tejashree08 <gandlatejashree08@gmail.com>
2026-09-21 14:22:28 +0530

    Add login feature

---
+++ app.py
@@ -0,0 +1,10 @@
+def login():
+    username = "student"
+    password = "1234"
+
+    if username == "student" and password == "1234":
+        print("Welcome Student")
+    else:
+        print("Invalid Login")
+
+login()
